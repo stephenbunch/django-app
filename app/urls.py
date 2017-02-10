d9b9378d44b99513db37211cd8c5cb641274b23e
Stephen Bunch <bunchclone@gmail.com>
2017-02-09 21:59:02 -0800

    Add home route

--- app/urls.py
+++ app/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.http import HttpResponse
+
+def home(req):
+    return HttpResponse('hello world')
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', home)
 ]
